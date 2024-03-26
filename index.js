dd6b34435700c93212160a23b09e81b879e26a42
eric mutuma <ericmutuma15@gmail.com>
2024-03-26 12:00:51 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (msg = "roller-skate") {
+    return `This Saturday, I want to ${msg}!`
+
+}
+
+function mondayWork (act = 'go to the office') {
+    return `This Monday, I will ${act}.`
+}
+
+
+const wrapAdjective = function (asterisk = "*", msg1 = "You are" ) {
+    return function (adjective) {
+        return `${msg1} ${asterisk}${adjective}${asterisk}!`
+    }
+
+}
